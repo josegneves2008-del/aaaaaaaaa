4214c66a5afd7cc00e7b7e92cad5e6244bea089f
josegneves2008-del <josegneves2008@gmail.com>
2026-04-16 18:04:35 +0100

    Create postcss.config.js with Tailwind and Autoprefixer configuration

---
+++ postcss.config.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const tailwindcss = require('tailwindcss');
+const autoprefixer = require('autoprefixer');
+
+module.exports = {
+  plugins: [
+    tailwindcss,
+    autoprefixer
+  ],
+};

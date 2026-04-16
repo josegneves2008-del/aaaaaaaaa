dfa9180ee620f8085b3eac76299cfccf089d0ff1
josegneves2008-del <josegneves2008@gmail.com>
2026-04-16 18:10:29 +0100

    Create Character component with headphones and listening animation

---
+++ src/components/Character.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Character.css';
+
+const Character = () => {
+  return (
+    <div className="character">
+      <div className="headphones">
+        <div className="earcup left"></div>
+        <div className="earcup right"></div>
+      </div>
+      <div className="wave"></div>
+    </div>
+  );
+};
+
+export default Character;
